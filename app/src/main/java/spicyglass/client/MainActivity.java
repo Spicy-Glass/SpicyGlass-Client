ba7f5ac26b530766d5b3e0d99eb14c79fa2f5708
The_Fireplace <the.f1repl4ce@gmail.com>
2020-03-02 13:31:29 -0600

    Initial project setup

---
+++ app/src/main/java/spicyglass/client/MainActivity.java
@@ -0,0 +1,21 @@
+package spicyglass.client;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import spicyglass.client.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
