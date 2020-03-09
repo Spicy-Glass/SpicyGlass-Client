53eb7f1045c0818a00ec357fc4d7ff26a9ac68e7
aaron_m_ramirez <amramirez1222@gmail.com>
2020-03-09 12:36:19 -0500

    had 1 button connected

--- app/src/main/java/spicyglass/client/MainActivity.java
+++ app/src/main/java/spicyglass/client/MainActivity.java
@@ -1,21 +1,42 @@
 package spicyglass.client;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import spicyglass.client.ui.main.MainFragment;
+import spicyglass.client.ui.screens.SettingActivity;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
+
+        /*if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
+                    .replace(R.id., MainFragment.newInstance())
                     .commitNow();
+        }*/
+
+            button = (Button) findViewById(R.id.setting);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openSettings();
+                }
+            });
         }
-    }
+        public void openSettings(){
+            Intent intent = new Intent (this, SettingActivity.class);
+            startActivity(intent);
+        }
+
 }
+
--- app/src/main/java/spicyglass/client/ui/main/MainViewModel.java
+++
@@ -1,7 +0,0 @@
-package spicyglass.client.ui.main;
-
-import androidx.lifecycle.ViewModel;
-
-public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
