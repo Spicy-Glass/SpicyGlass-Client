83219be25b2bca366097150b08096ee6ac6fa6a6
aaron_m_ramirez <amramirez1222@gmail.com>
2020-03-21 12:25:35 -0600

    Finished auto start and the locker button. Overall all functionalities are mostly done I will be working on this again when I get back to lubbock. the only things needed to be fixed/changed would be adding vehicles and optimizing it for 2 door cars/trucks

--- app/src/main/java/spicyglass/client/ui/screens/AutoStartActivity.java
+++ app/src/main/java/spicyglass/client/ui/screens/AutoStartActivity.java
@@ -1,15 +1,45 @@
 package spicyglass.client.ui.screens;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import spicyglass.client.R;
 
-public class AutoStartActivity extends AppCompatActivity {
+public class AutoStartActivity extends AppCompatActivity implements View.OnClickListener {
+    Button Start, Stop;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.auto_page);
+
+        Start = (Button) findViewById(R.id.AutoStartBut);
+        Start.setOnClickListener(this);
+        Stop = (Button) findViewById(R.id.AutoStopBut);
+        Stop.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.AutoStartBut:
+                Start.setVisibility(View.INVISIBLE);
+                Stop.setVisibility(View.VISIBLE);
+                Start.setEnabled(false);
+                Stop.setEnabled(true);
+                break;
+
+            case R.id.AutoStopBut:
+                Start.setVisibility(View.VISIBLE);
+                Stop.setVisibility(View.INVISIBLE);
+                Start.setEnabled(true);
+                Stop.setEnabled(false);
+                break;
+        }
+
     }
 }
